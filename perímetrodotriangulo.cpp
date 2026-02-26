742fc22671c5ee229afa2dd23d6f94c52dc4a15c
Vaccation17 <leonardo.vaccari.rodrigues@escola.pr.gov.br>
2026-02-26 15:04:52 -0300

    Add files via upload

---
+++ perímetrodotriangulo.cpp
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Six Seven";
+    cout<<"\nSix Seven";
+    
+    
+    // Tipo Inteiro
+
+   int a;
+   int b;
+   int c;
+   
+   cout<< "\nDigite o valor de a:";
+   cin>> a;
+   cout<< "\nDigite o valor de b:";
+   cin>> b;
+   cout<< "\nDigite o valor de c:";
+   cin>> c;
+   int soma = a+b+c;
+
+   cout << "\nSoma = "<< soma;
+    return 0;
+}
+
