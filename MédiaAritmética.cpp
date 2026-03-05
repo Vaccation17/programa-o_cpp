a7865c9c9b23bbc8641e0a86a0ecce4218a1da1d
Vaccation17 <leonardo.vaccari.rodrigues@escola.pr.gov.br>
2026-03-05 15:08:01 -0300

    Add files via upload

---
+++ MédiaAritmética.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    double n1, n2, n3 , ma;
+    
+    cout<<"Insira a nota do 1° Trimestre: ";
+    cin>>n1;
+    cout<<"Insira a nota do 2° Trimestre: ";
+    cin>>n2;
+    cout<<"Insira a nota do 3° Trimestre: ";
+    cin>>n3;
+    
+    ma = (n1+n2+n3)/3;
+    cout<<"Média="<<ma;
+    
+    return 0;
+}
